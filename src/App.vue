0d6fdc38204f4d1d91f7fb88c7747ce0c8e3d67f
Ahmed Osama <70448761+ahmeddossamaa@users.noreply.github.com>
2021-09-06 16:27:24 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <topBar appName="CRUD - Vue.Js"/>
+  <itemsForm/>
+  <foot/>
+</template>
+
+<script>
+
+import topBar from './components/topBar.vue'
+import itemsForm from './components/itemsForm.vue'
+import foot from './components/foot.vue'
+
+export default {
+  name: 'App',
+  components: {
+    topBar,
+    itemsForm,
+    foot,
+  },
+}
+</script>
